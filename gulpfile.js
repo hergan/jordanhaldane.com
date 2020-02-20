5d48d4f1704b12365e5c660f267a669d2ebc7b42
Jordan Haldane <haldajo@gmail.com>
2020-02-19 18:48:35 -0600

    :art: Added framework for temporary landing page.

---
+++ gulpfile.js
@@ -0,0 +1,75 @@
+"use strict";
+
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var postcss = require("gulp-postcss");
+var cssnano = require("cssnano");
+var sassGlob = require("gulp-sass-glob");
+var del = require("del");
+var browserSync = require("browser-sync");
+
+sass.compiler = require("node-sass");
+
+const server = browserSync.create();
+
+var paths = {
+	app: {
+		src: "./",
+		dest: "./"
+	},
+	appIndex: {
+		src: "./index.html",
+		dest: "./index.html"
+	},
+	styles: {
+		src: "./scss/**/*.scss",
+		dest: "./css/"
+	},
+	scripts: {
+		src: "./src/scripts/**/*.js",
+		dest: "./assets/scripts/"
+	}
+};
+
+const clean = () => del(["paths.app.src" + "assets/"]);
+
+function styles(cb) {
+	var plugins = [cssnano()];
+	return gulp
+		.src(paths.styles.src)
+		.pipe(sassGlob())
+		.pipe(sass())
+		.on("error", sass.logError)
+		.pipe(postcss(plugins))
+		.pipe(gulp.dest(paths.styles.dest));
+	cb();
+}
+
+function reload(done) {
+	server.reload();
+	done();
+}
+
+function serve(done) {
+	server.init({
+		server: {
+			baseDir: paths.app.src
+		}
+	});
+	done();
+}
+
+function build(done) {
+	cssnano({ autoprefixer: false });
+	done();
+}
+
+const cssWatch = () => gulp.watch([paths.styles.src, paths.appIndex.src], gulp.series(styles, reload));
+
+const dev = gulp.series(clean, styles, serve, cssWatch);
+
+exports.styles = styles;
+exports.build = build;
+exports.reload = reload;
+exports.clean = clean;
+exports.default = dev;
