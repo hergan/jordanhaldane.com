dba84793b41c51fb8e4d5259cd347985f2a356cc
Jordan Haldane <haldajo@gmail.com>
2020-06-19 17:21:50 -0500

    :art: organized sass. :sparkles: added projects

--- gulpfile.js
+++ gulpfile.js
@@ -13,58 +13,60 @@ sass.compiler = require("node-sass");
 const server = browserSync.create();
 
 var paths = {
-	app: {
-		src: "./",
-		dest: "./"
-	},
-	appIndex: {
-		src: "./index.html",
-		dest: "./index.html"
-	},
-	styles: {
-		src: "./scss/**/*.scss",
-		dest: "./css/"
-	},
-	scripts: {
-		src: "./src/scripts/**/*.js",
-		dest: "./assets/scripts/"
-	}
+    app: {
+        src: "./",
+        dest: "./"
+    },
+    appIndex: {
+        src: "./*.html",
+        watch: "./*/*.html",
+        dest: "./index.html"
+    },
+    styles: {
+        src: "./scss/**/style.scss",
+        watch: "./scss/**/*.scss",
+        dest: "./css/"
+    },
+    scripts: {
+        src: "./src/scripts/**/*.js",
+        dest: "./assets/scripts/"
+    }
 };
 
 const clean = () => del(["paths.app.src" + "assets/"]);
 
 function styles(cb) {
-	var plugins = [cssnano()];
-	return gulp
-		.src(paths.styles.src)
-		.pipe(sassGlob())
-		.pipe(sass())
-		.on("error", sass.logError)
-		.pipe(postcss(plugins))
-		.pipe(gulp.dest(paths.styles.dest));
-	cb();
+    var plugins = [cssnano()];
+    return gulp
+        .src(paths.styles.src)
+        .pipe(sassGlob())
+        .pipe(sass())
+        .on("error", sass.logError)
+        .pipe(postcss(plugins))
+        .pipe(gulp.dest(paths.styles.dest));
+    cb();
 }
 
 function reload(done) {
-	server.reload();
-	done();
+    server.reload();
+    done();
 }
 
 function serve(done) {
-	server.init({
-		server: {
-			baseDir: paths.app.src
-		}
-	});
-	done();
+    server.init({
+        server: {
+            baseDir: paths.app.src
+        }
+    });
+    done();
 }
 
 function build(done) {
-	cssnano({ autoprefixer: false });
-	done();
+    cssnano({ autoprefixer: false });
+    done();
 }
 
-const cssWatch = () => gulp.watch([paths.styles.src, paths.appIndex.src], gulp.series(styles, reload));
+const cssWatch = () => gulp.watch([paths.styles.watch, paths.appIndex.src, paths.appIndex.watch], gulp.series(styles, reload));
 
 const dev = gulp.series(clean, styles, serve, cssWatch);
 
